42efa4232109350d0287c9492de08ba1ab83902d
Дмитрий Голубков <dgolubkov@devteam.local>
2022-08-14 17:57:51 +0300

    init

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+exports.get_hero_stats = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
+
+exports.set_hero_stats = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
+
+
+exports.set_hero_stats = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
+
+
+exports.upload_hero_image = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
+
+exports.get_hero_image = (req, res) => {
+    res.send('NOT IMPLEMENTED');
+}
